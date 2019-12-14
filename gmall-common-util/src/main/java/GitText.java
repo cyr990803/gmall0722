26959ef0e845ce704fcd4472b110c7f793d193a4
cyr990803 <1306032496@qq.com>
2019-12-14 10:54:02 +0800

    嗡嗡鲸来了

---
+++ gmall-common-util/src/main/java/GitText.java
@@ -0,0 +1,5 @@
+public class GitText {
+    public void hello(){
+        System.out.println("江小皮来啦");
+    }
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.user.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+
+    public String index(){
+        return "index";
+    }
+}
